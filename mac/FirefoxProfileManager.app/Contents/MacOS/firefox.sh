910a8b695166bd150bf182eb6e604623a7c91b49
okazuya <okazuya@localhost>
2012-10-20 08:05:15 +0900

    mac用とwinxp用を追加。

---
+++ mac/FirefoxProfileManager.app/Contents/MacOS/firefox.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+/Applications/Firefox.app/Contents/MacOS/firefox -p &
+exit 0
---
+++ winxp/firefox.bat
@@ -0,0 +1,3 @@
+@echo off
+SET MOZ_NO_REMOTE=true
+"c:\Program Files\Mozilla Firefox\firefox.exe" -P %1 %2 %3
